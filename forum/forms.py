a0cc782c3a4a9a09a57ec5d32373513eccad028a
sergor <sergey13gorbenko@gmail.com>
2018-12-20 14:36:23 +0200

    backend 0.4 registration

--- forum/forms.py
+++ forum/forms.py
@@ -1,8 +1,11 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
-class Registration(forms.Form):
-    username = forms.CharField(label='username', max_length=50)
-    email = forms.EmailField(label='email', )
-    password = forms.PasswordInput()
-    repassword = forms.PasswordInput()
+class Registration(UserCreationForm):
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
