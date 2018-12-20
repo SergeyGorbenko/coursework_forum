8e23244a4603ba307e1698777f8d12b51b5236fd
sergor <sergey13gorbenko@gmail.com>
2018-12-20 17:22:15 +0200

    backend 0.7 registration

--- forum/views.py
+++ forum/views.py
@@ -1,40 +1,34 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from forum.models import *
 
 # Create your views here.
 from forum.forms import Registration
 
 
-def home(request):
+def index(request):
     context = {}
     return render(request, 'index.html', context=context)
 
 
 def registration(request):
-    context = {}
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('')
+    if request.method == 'GET':
+        form = Registration()
+        return render(request, 'registration/registration.html', {'form': form})
     if request.method == 'POST':
         form = Registration(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            pass_1 = form.cleaned_data['password1']
-            pass_2 = form.cleaned_data['password2']
-            context['profile'] = {'username': username,
-                                  'email': email,
-                                  'pass_1': pass_1,
-                                  'pass_2': pass_2}
-            try:
-                if User.objects.get(username=username) and User.objects.get(email=email):
-                    context['error'] = ['user_exist']
-            except User.DoesNotExist:
-                if pass_1 != pass_2:
-                    context['error'] = ['passwords_not_match']
-                else:
-                    user = User(username=username, email=email, password=pass_1)
-                    user.save()
-                    HttpResponseRedirect('/login/')
-
-    context['form'] = Registration()
-    return render(request, 'registration/registration.html', context=context)
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('/post/')
+        else:
+            # If there were errors, we render the form with these
+            # errors
+            return render(request, 'registration/registration.html', {'form': form})
+    return render(request, 'registration/registration.html', {'form': Registration()})
