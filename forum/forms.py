63f6b0390a85dba30b0cfec88a624225419cba3b
sergor <sergey13gorbenko@gmail.com>
2018-12-20 14:33:05 +0200

    backend 0.4 registration

--- forum/forms.py
+++ forum/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 class Registration(forms.Form):
     username = forms.CharField(label='username', max_length=50)
-    email = forms.EmailField()
+    email = forms.EmailField(label='email', )
     password = forms.PasswordInput()
     repassword = forms.PasswordInput()
